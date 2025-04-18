7bfe9c5d3c0c5e325bcebd40de281f64e6203a3b
dineshelegit@gmail.com <dklive7755@linux.myguest.virtualbox.org>
2025-04-18 23:07:52 +0530

    structure padding

--- learning/structure/assignment1.c
+++ learning/structure/assignment1.c
@@ -10,7 +10,7 @@ int main()
 	struct student s1 = {1,"Dinesh",8.8},s2 = {2,"Kumar",9.9},s3 = {3,"Ruthra",9.8};
 	
 	printf("\tRoll_Number\tName\tCGPA\n");
-	printf("\t%d\t%s\t%f\n",s1.roll_number,s1.name,s1.cgpa);
-	printf("\t%d\t%s\t%f\n",s1.roll_number,s1.name,s1.cgpa);
-	printf("\t%d\t%s\t%f\n",s1.roll_number,s1.name,s1.cgpa); 
+	printf("\t%d\t\t%s\t%.2f\n",s1.roll_number,s1.name,s1.cgpa);
+	printf("\t%d\t\t%s\t%.2f\n",s2.roll_number,s2.name,s2.cgpa);
+	printf("\t%d\t\t%s\t%.2f\n",s3.roll_number,s3.name,s3.cgpa); 
 }
